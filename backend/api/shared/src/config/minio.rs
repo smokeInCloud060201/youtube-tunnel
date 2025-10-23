f931b02c6c6498407d42e5fbc4f09344daf2ab0f
Ngoc Khanh <khanhk56a12@gmail.com>
2025-10-23 11:22:54 +0700

    Test

--- backend/api/shared/src/config/minio.rs
+++ backend/api/shared/src/config/minio.rs
@@ -1,24 +1,41 @@
-use aws_config::meta::region::RegionProviderChain;
-use aws_config::{BehaviorVersion, Region};
+use aws_config::{BehaviorVersion, Region as S3Region};
 use aws_sdk_s3::Client;
 use std::env;
+use aws_sdk_s3::config::{Builder, Credentials};
 use tracing::info;
 
 pub async fn init() -> anyhow::Result<Client> {
     let aws_region = env::var("AWS_REGION").unwrap_or_else(|_| "us-east-1".into());
     let minio_url = env::var("MINIO_ENDPOINT").expect("ENDPOINT must be set");
+    let minio_username = env::var("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID must be set");
+    let minio_password = env::var("AWS_SECRET_ACCESS_KEY").expect("AWS_SECRET_ACCESS_KEY must be set");
 
-    let region = RegionProviderChain::first_try(Region::new(aws_region));
 
-    let config = aws_config::defaults(BehaviorVersion::latest())
-        .region(region)
-        .endpoint_url(&minio_url)
+    let base_config = aws_config::defaults(BehaviorVersion::latest())
+        .region(S3Region::new(aws_region.clone()))
         .load()
         .await;
 
+
+    let creds = Credentials::new(
+        minio_username,
+        minio_password,
+        None,
+        None,
+        "static",
+    );
+
+    let config = Builder::from(&base_config)
+        .region(S3Region::new(aws_region))
+        .endpoint_url(&minio_url)
+        .credentials_provider(creds)
+        .force_path_style(true)
+        .build();
+
+
     info!("Init MINIO with {}", minio_url);
 
-    let client = Client::new(&config);
+    let client =  Client::from_conf(config);
 
     init_bucket(&client, String::from("videos")).await?;
     init_bucket(&client, String::from("credential")).await?;
@@ -29,8 +46,10 @@ pub async fn init() -> anyhow::Result<Client> {
 async fn init_bucket(client: &Client, bucket_name: String) -> anyhow::Result<()> {
     if !bucket_exists(client, &bucket_name).await? {
         info!("Creating bucket with name {}", bucket_name);
-
-        client.create_bucket().bucket(&bucket_name).send().await?;
+        if let Err(e) = client.create_bucket().bucket(&bucket_name).send().await {
+            tracing::error!("Failed to create bucket {}: {:?}", bucket_name, e);
+            return Err(anyhow::anyhow!(e));
+        }
         info!("Created bucket {} successfully", bucket_name);
     } else {
         info!("Bucket {} already exists", bucket_name);
@@ -39,10 +58,13 @@ async fn init_bucket(client: &Client, bucket_name: String) -> anyhow::Result<()>
     Ok(())
 }
 
-async fn bucket_exists(client: &Client, bucket_name: &String) -> anyhow::Result<bool> {
-    let buckets = client.list_buckets().send().await?;
-    Ok(buckets
+async fn bucket_exists(client: &Client, bucket_name: &str) -> anyhow::Result<bool> {
+    let resp = client.list_buckets().send().await?;
+
+    let exists = resp
         .buckets()
         .iter()
-        .any(|item| item.name().unwrap_or_default() == bucket_name))
+        .any(|b| b.name().unwrap_or_default() == bucket_name);
+
+    Ok(exists)
 }
