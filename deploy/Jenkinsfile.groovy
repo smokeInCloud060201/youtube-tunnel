f656eb9dabb1e0536b469b07632ffb9bf3acc3fe
Ngoc Khanh <khanhk56a12@gmail.com>
2025-12-18 13:16:26 +0700

    Redeploy at new server

--- deploy/Jenkinsfile.groovy
+++ deploy/Jenkinsfile.groovy
@@ -1,5 +1,10 @@
 pipeline {
-    agent any
+    agent {
+        docker {
+            image 'docker:26.1.3'
+            args '-v /var/run/docker.sock:/var/run/docker.sock'
+        }
+    }
 
     environment {
         IMAGE_TAG  = "latest"
